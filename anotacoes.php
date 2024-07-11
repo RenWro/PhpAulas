c048b6f7b567ab6c31825bbb620e0ef2fddcd5e3
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-07-11 09:14:58 -0300

    Add files via upload

---
+++ anotacoes.php
@@ -0,0 +1,12 @@
+/*
+
+Php significa = PHP é um acrônimo recursivo para PHP: Hypertext Preprocessor
+(Pré-Processador de Hipertexto), que originalmente se chamava Personal Home Page
+(Página Inicial Pessoal). Ele também é um subconjunto de linguagens de scripts como JavaScript
+e Python.
+
+Ainda que seja possível programar front-end nele, se estabeleceu que ele funciona muito mais pelo lado do
+servidor do que na máquina do cliente
+Máquina virtual básica = HD SSE 80Gb, Ram 4Gb e 2CPUvs
+
+*/
